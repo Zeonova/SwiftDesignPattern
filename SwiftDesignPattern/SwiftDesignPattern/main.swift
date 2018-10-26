5d7cc119f3c00e15faa7c22c1aae6c299f55da39
zhangwei <zhangwei@qiushicaifu.com>
2018-10-26 21:38:40 +0800

    commit:init

---
+++ SwiftDesignPattern/SwiftDesignPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDesignPattern
+//
+//  Created by zhangwei on 2018/10/26.
+//  Copyright © 2018年 Mr.Z. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
